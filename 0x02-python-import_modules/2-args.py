a36fbd5bd124d30cf09f5965646263b39395853a
UCIX210 <javier.uc@outlook.com>
2022-04-28 22:08:32 -0500

    0x02 2.3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,13 @@ if __name__ == "__main__":
     import sys
     num_arguments = len(sys.argv) - 1
 
-    char_s = ""
+    char_s = ":"
     if num_arguments > 1:
-        char_s = "s"
+        char_s = "s:"
     elif num_arguments == 0:
         char_s = "s."
 
-    print(f"{num_arguments} argument{char_s}:")
+    print(f"{num_arguments} argument{char_s}")
 
     if num_arguments > 0:
         for argument in range(1, num_arguments + 1):
