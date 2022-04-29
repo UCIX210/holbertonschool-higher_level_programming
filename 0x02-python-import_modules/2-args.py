7bcb62fba2b61f1705dbe319f887c3122c095bae
UCIX210 <javier.uc@outlook.com>
2022-04-28 22:05:34 -0500

    0x02 2.1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    num_arguments = len(sys.argv) -1
+    num_arguments = len(sys.argv) - 1
 
     char_s = ""
     if num_arguments > 1:
