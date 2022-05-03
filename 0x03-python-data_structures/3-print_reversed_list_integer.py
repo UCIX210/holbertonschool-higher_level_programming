44077e2b67a192e3a883217cf73fe450c3059e1c
UCIX210 <javier.uc@outlook.com>
2022-05-02 23:14:29 -0500

    0x03 3.6

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if my_list:
         if len(my_list) > 0:
             for i in my_list[::-1]:
-                print(f"{i}")
+                print("{:d}".format(i))
