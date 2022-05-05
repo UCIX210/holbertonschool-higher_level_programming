5ca7335d374595242c701f39deab445ddd6d651e
UCIX210 <javier.uc@outlook.com>
2022-05-04 22:26:10 -0500

    0X04 1.2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,8 +5,3 @@ def search_replace(my_list, search, replace):
         if list2[i] == search:
             list2[i] = replace
     return (list2)
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
