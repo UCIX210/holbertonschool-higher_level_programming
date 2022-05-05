b08e1da11cde9cd9cab287785e9d8f6227d28583
UCIX210 <javier.uc@outlook.com>
2022-05-04 23:10:54 -0500

    0X04 6.2

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for i in sorted(a_dictionary):
-        print(f"{str(a_dictionary[i])}:")
+        print(f"{str(i)}:", a_dictionary[i])
