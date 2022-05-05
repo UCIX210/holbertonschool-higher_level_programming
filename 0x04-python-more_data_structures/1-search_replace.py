06c85a73dab177506529c67a5096c9dc1c312bdf
UCIX210 <javier.uc@outlook.com>
2022-05-04 22:25:43 -0500

    0X04 1.2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,12 @@
-#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    list2 = my_list.copy()
-    list2[search] = replace
-    return list2
+    """Replace all occurrences of an element by another in a new list."""
+    list2 = my_list
+    for i in range(len(list2)):
+        if list2[i] == search:
+            list2[i] = replace
+    return (list2)
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
