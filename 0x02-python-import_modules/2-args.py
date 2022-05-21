4fce259d83ab2a998d5be919edb466bea8ae9108
UCIX210 <javier.uc@outlook.com>
2022-05-21 16:12:26 -0500

    0x02 2c

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,7 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
-        print("1 argument:")
-        print("1:", argv[1])
+        print("1 argument:\n1:", argv[1])
     else:
         print(f"{len(argv)-1} arguments:")
         for i in range(1, len(argv)):
