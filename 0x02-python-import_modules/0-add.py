a705abf37e02755ae45feec990e55461b6abf11a
UCIX210 <javier.uc@outlook.com>
2022-05-21 12:02:11 -0500

    0x02 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """program that imports and funciton and prints the result"""
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+import add_0
+a = 1
+b = 2
+print(f"{a} + {b} = {add_0.add(a, b)}")
