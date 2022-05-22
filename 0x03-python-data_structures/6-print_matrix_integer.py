c76d48814da584facacf9f19b3c62be7ab90fc6d
UCIX210 <javier.uc@outlook.com>
2022-05-22 18:47:38 -0500

    0x03 6.3

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for index in matrix:
-        print(" ".join("{:d}".format(x) for x in index))
+        for x in index:
+            print(" ".join("{:d}".format(x)))
