423f39fed86faceac4e0606978fc0a30a6502328
UCIX210 <javier.uc@outlook.com>
2022-05-31 19:17:32 -0500

    0x0b 7

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,6 +12,6 @@ def save_to_json_file(my_obj, filename):
     """
     Escribir un objeto en un archivo de texto, usando un formato JSON
     """
-    with open(filename, 'a') as f:
+    with open(filename, 'w') as f:
         f.write(json.dumps(my_obj))
     return 0
