28568d15fac29b84e8165f2014f061ae76cb2710
UCIX210 <javier.uc@outlook.com>
2022-06-02 23:36:36 -0500

    all code 0x08

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,3 +48,19 @@ class Square:
         for i in range(self.__size):
             print(" " * self.__position[0], end="")
             print("#" * self.__size)
+
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (0, None))
+my_square_3.my_print()
+
+print("--")
